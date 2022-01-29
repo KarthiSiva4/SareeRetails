f6b0378ca9492afcdf9000a0f33b6903bcb98c0c
Siva <karthi.siva@accenture.com>
2022-01-30 00:19:15 +0530

    favicon added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.css';
+import logo from './logo.png';
+import background from './background.jpg';
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="C:\Users\Public\looka.com_editor_82147617 (1).png" alt="" width="30" height="24" class="d-inline-block align-text-top"></img>
-      Bootstrap
-    </a>
-  </div>
-</nav>
-        <p>
-          I Know..
-        </p>
-      </header>
+    <div class="jumbotron">
+    <div class="container">
+        <h1>Hello, world!</h1>
+    </div>
     </div>
   );
 }
